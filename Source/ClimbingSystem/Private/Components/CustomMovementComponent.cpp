80553eaaa7bf9675d96bb4ef392d4733486318c1
rogan <mhpark@olimplanet.com>
2024-05-31 16:27:44 +0900

    Climb Trace

--- Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
@@ -1,6 +1,53 @@
 #include "ClimbingSystem/Public/Components/CustomMovementComponent.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(CustomMovementComponent)
 
 UCustomMovementComponent::UCustomMovementComponent()
 {
 }
+
+void UCustomMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TraceClimbableSurfaces();
+}
+#pragma region ClimbTraces
+TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End,
+	bool bShowDebugShape)
+{
+	TArray<FHitResult> OutCapsuleTraceHitResults;
+	
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		ClimbCapsuleTraceRadius,
+		ClimbCapsuleTraceHalfHeight,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResults,
+		false
+		);
+
+	return OutCapsuleTraceHitResults;
+}
+#pragma endregion
+
+#pragma region ClimbCore
+
+void UCustomMovementComponent::TraceClimbableSurfaces()
+{
+	const FVector StartOffset = UpdatedComponent->GetForwardVector() * 30.f;
+	const FVector Start = UpdatedComponent->GetComponentLocation() + StartOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector();
+	
+	DoCapsuleTraceMultiByObject(Start,End,true);
+}
+
+#pragma endregion
--- Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
+++ Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
@@ -13,4 +13,29 @@ class CLIMBINGSYSTEM_API UCustomMovementComponent : public UCharacterMovementCom
 
 public:
 	UCustomMovementComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+#pragma region ClimbTraces
+
+	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false);
+#pragma endregion
+
+#pragma region ClimbCore
+	
+	void TraceClimbableSurfaces();
+	
+#pragma endregion
+
+#pragma region ClimbVariables
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement : Climbing", meta = (AllowPrivateAccess = "true"))
+	TArray<TEnumAsByte<EObjectTypeQuery>> ClimbableSurfaceTraceTypes;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement : Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceRadius = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement : Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceHalfHeight = 72.f;
+#pragma endregion
 };
