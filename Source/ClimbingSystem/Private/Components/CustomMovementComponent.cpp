bbd7d0b0c15f883379d6f10fb3fe76d844420da4
rogan <mhpark@olimplanet.com>
2024-05-31 16:51:12 +0900

    Eye Height Trace

--- Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
@@ -1,5 +1,6 @@
 #include "ClimbingSystem/Public/Components/CustomMovementComponent.h"
 
+#include "GameFramework/Character.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(CustomMovementComponent)
@@ -14,6 +15,7 @@ void UCustomMovementComponent::TickComponent(float DeltaTime, ELevelTick TickTyp
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	TraceClimbableSurfaces();
+	TraceFromEyeHeight(100.f);
 }
 #pragma region ClimbTraces
 TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End,
@@ -37,6 +39,25 @@ TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const F
 
 	return OutCapsuleTraceHitResults;
 }
+
+FHitResult UCustomMovementComponent::DoLineTraceSingleByObject(const FVector& Start, const FVector& End,
+	bool bShowDebugShape)
+{
+	FHitResult OutHit;
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutHit,
+		false
+		);
+
+	return OutHit;
+}
 #pragma endregion
 
 #pragma region ClimbCore
@@ -48,6 +69,17 @@ void UCustomMovementComponent::TraceClimbableSurfaces()
 	const FVector End = Start + UpdatedComponent->GetForwardVector();
 	
 	DoCapsuleTraceMultiByObject(Start,End,true);
+}
+
+void UCustomMovementComponent::TraceFromEyeHeight(float TraceDistance, float TraceStartOffset)
+{
+	const FVector ComponentLocation = UpdatedComponent->GetComponentLocation();
+	const FVector EyeHeightOffset = UpdatedComponent->GetUpVector() * (CharacterOwner->BaseEyeHeight + TraceStartOffset);
+	
+	const FVector Start = ComponentLocation + EyeHeightOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector() * TraceDistance;
+
+	DoLineTraceSingleByObject(Start,End,true);
 }
 
 #pragma endregion
