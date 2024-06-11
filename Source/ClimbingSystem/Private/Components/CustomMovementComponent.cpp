f98ceaa27c125f5d14b554e514268fc66fe3d039
online5880 <gjtjqkr1@naver.com>
2024-06-11 15:37:21 +0900

    Section Wrap Up

--- Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
@@ -149,7 +149,7 @@ bool UCustomMovementComponent::TraceClimbableSurfaces()
 	const FVector Start = UpdatedComponent->GetComponentLocation() + StartOffset;
 	const FVector End = Start + UpdatedComponent->GetForwardVector();
 	
-	ClimbableSurfacesTracedResults = DoCapsuleTraceMultiByObject(Start,End,true);
+	ClimbableSurfacesTracedResults = DoCapsuleTraceMultiByObject(Start,End);
 
 	return !ClimbableSurfacesTracedResults.IsEmpty();
 }
@@ -262,8 +262,7 @@ bool UCustomMovementComponent::CheckShouldStopClimbing() const
 	{
 		return true;
 	}
-
-	Debug::Print(TEXT("Degree Diff : ") + FString::SanitizeFloat(DegreeDiff),FColor::Cyan,1);
+	
 	return false;
 }
 
@@ -305,12 +304,7 @@ void UCustomMovementComponent::ToggleClimbing(bool bEnableClimb)
 		if(CanStartClimbing())
 		{
 			//Enter the climb state
-			Debug::Print(TEXT("Can start climbing"));
 			StartClimbing();
-		}
-		else
-		{
-			Debug::Print(TEXT("Can NOT start climbing"));
 		}
 	}
 	else
