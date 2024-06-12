1dbfd90905b575ddea3a5e3ef192a5eb90d697b5
online5880 <gjtjqkr1@naver.com>
2024-06-12 22:36:14 +0900

    AnimInstanceDone

---
+++ Source/ClimbingSystem/Public/AnimInstance/CharacterAnimInstance.h
@@ -0,0 +1,44 @@
+#pragma once
+
+
+#include "Animation/AnimInstance.h"
+#include "CharacterAnimInstance.generated.h"
+
+class UCustomMovementComponent;
+class AClimbingSystemCharacter;
+/**
+ * 
+ */
+UCLASS()
+class CLIMBINGSYSTEM_API UCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<AClimbingSystemCharacter> ClimbingSystemCharacter;
+
+	UPROPERTY()
+	TObjectPtr<UCustomMovementComponent> CustomMovementComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta = (AllowPrivateAccess = "true"))
+	float GroundSpeed;
+	void GetGroundSpeed();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta = (AllowPrivateAccess = "true"))
+	float AirSpeed;
+	void GetAirSpeed();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta = (AllowPrivateAccess = "true"))
+	bool bShouldMove;
+	void GetShouldMove();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta = (AllowPrivateAccess = "true"))
+	bool bIsFalling;
+	void GetIsFalling();
+};
